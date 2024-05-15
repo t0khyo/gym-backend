b38e115f008ec54911bc67a535402c07588fee2a
HassanAlwakeel1 <hassanalwakeel911@gmail.com>
2024-05-15 13:37:19 +0300

    updated

--- src/main/java/com/anonymous/gym/helper/DataLoader.java
+++ src/main/java/com/anonymous/gym/helper/DataLoader.java
@@ -1,8 +1,11 @@
 package com.anonymous.gym.helper;
 
 import com.anonymous.gym.model.entity.Exercise;
+import com.anonymous.gym.model.entity.Meal;
+import com.anonymous.gym.model.entity.Nutrients;
 import com.anonymous.gym.model.entity.WorkoutPlan;
 import com.anonymous.gym.model.entity.enums.ExerciseDifficulty;
+import com.anonymous.gym.model.entity.enums.MealType;
 import com.anonymous.gym.repository.ExerciseRepository;
 import com.anonymous.gym.repository.MealRepository;
 import com.anonymous.gym.repository.WorkoutPlanRepository;
@@ -71,5 +74,32 @@ public class DataLoader {
         workoutPlanRepository.saveAll(workoutPlans);
     }
 
+    private void loadMeals() {
+        List<Meal> meals = List.of(
+                Meal.builder()
+                        .name("Tomato and Feta Baked Eggs")
+                        .description("Smoky roasted tomatoes, salty feta, and sunny eggs all bake together on a single sheet pan! Spoon it on top of toast for a crowd-pleasing (and hassle-free) brunch.")
+                        .type(MealType.BREAKFAST)
+                        .calories(300)
+                        .nutrients(new Nutrients(25, 40, 5))
+                        .build(),
+                Meal.builder()
+                        .name("Peanut Chicken Protein Bowls")
+                        .description("Preheat oven to 425°. On a large baking sheet, toss potatoes and onion with 1 tablespoon olive oil; season with salt and pepper. Bake until tender, 20 to 25 minutes")
+                        .type(MealType.LUNCH)
+                        .calories(550)
+                        .nutrients(new Nutrients(50,24,10))
+                        .build()
+        );
 
+        List<Meal> meals1 = List.of(
+                Meal.builder()
+                        .name("Scrambled Eggs with Smoked Trout and Crème Fraîche")
+                        .description("Easy breakfasts don’t have to be boring. With some simple techniques (low and slow makes for the fluffiest eggs!) and a little smoked trout, this scramble makes my a.m. extra elegant! — Associate Food Editor Becca Miller")
+                        .type(MealType.BREAKFAST)
+                        .calories(400)
+                        .nutrients(new Nutrients(30,34,7))
+                        .build()
+        );
+    }
 }
